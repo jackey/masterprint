d0a847482fd030a71543b806559b2694a6308b70
Jackey chen <jziwenchen@gmail.com>
2012-10-05 08:23:37 +0800

    添加一个安装脚本，暂时很简单；只是不想每次安装都敲繁琐的命令

---
+++ profiles/masterprint/install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+drush si masterprint --account-mail=jziwenchen@gmail.com --account-name=admin --account-pass=admin --site-mail=jziwenchen@gmail.com --site-name="master print"
+
+#drush si masterprint -y
