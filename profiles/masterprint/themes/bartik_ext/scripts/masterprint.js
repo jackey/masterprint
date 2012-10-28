9cb1a0dc9d4fe5825e20a5bb9a44cb59e0fc0441
Jackey chen <jziwenchen@gmail.com>
2012-10-28 11:27:17 +0800

    增加了产品环节

--- profiles/masterprint/themes/bartik_ext/scripts/masterprint.js
+++ profiles/masterprint/themes/bartik_ext/scripts/masterprint.js
@@ -5,6 +5,25 @@
 	  attach: function (context, settings) {
 	    //隐藏修改用户的edit profile secodory tab
 	    $('body.page-user-edit .secondary').hide();
+
+	    $('.showProductThumbnail').click(function () {
+	    	var src = $(this).attr('href');
+	    	var img = $("<img />").attr('src', src);
+	    	console.log(img);
+	    	try {
+	    		var maxBody = $('body').height();
+	    		$.modal(img, {
+	    			maxHeight: 600,
+	    			maxWidth: 700,
+					opacity:80,
+					overlayCss: {backgroundColor:"#000"}
+	    		});
+	    	}
+	    	catch (e) {
+
+	    	}
+	    	return false;
+	    });
 	  }
 	};
 })(jQuery);
