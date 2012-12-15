3e51de27debb9b18840645571258fb0c53da5af1
jackniu <jackniu8@gmail.com>
2012-12-15 16:20:37 +0800

    update css

--- profiles/masterprint/themes/bartik_ext/templates/menu_local_tasks.tpl.php
+++ profiles/masterprint/themes/bartik_ext/templates/menu_local_tasks.tpl.php
@@ -1,6 +1,8 @@
+<?php if ($primary): ?>
 <ul id="primary-tab" class="tabs primary">
 	<?php print drupal_render($primary)?>
 </ul>
+<?php endif; ?>
 <div id="welcome-message">
 	<div class="inner">
         <div class="left">您好！MasterPrint 华方印务欢迎您的访问！</div>
@@ -13,6 +15,8 @@
   <div id="breadcrumb"><?php print $breadcrumb; ?></div>
 <?php endif; ?>
 
+<?php if ($secondary): ?>
 <ul id="secondary-tab" class="tabs secondary">
 	<?php print drupal_render($secondary)?>
 </ul>
+<?php endif; ?>
