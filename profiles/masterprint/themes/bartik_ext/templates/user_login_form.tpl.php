0256812534510d8f2e378ebd74f407144fc7006c
Jackey chen <jziwenchen@gmail.com>
2012-12-16 22:06:06 +0800

    ds

--- profiles/masterprint/themes/bartik_ext/templates/user_login_form.tpl.php
+++ profiles/masterprint/themes/bartik_ext/templates/user_login_form.tpl.php
@@ -8,7 +8,7 @@
 
 <div class="actions">
 	<div class="links forget-password">
-		<?php print l(t('忘记密码'), 'user/password')?>
+		<?php //print l(t('忘记密码'), 'user/password')?>
 	</div>
 	<?php print drupal_render($form['actions'])?>
 </div>
